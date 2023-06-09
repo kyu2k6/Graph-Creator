b5db7bd1f615a3ab4a48ac0161f03e6bde417857
U-BSD\411915 <411915@SHS-HJ86ND2.beaverton.k12.or.us>
2023-06-09 14:18:46 -0700

    3

--- main.cpp
+++ main.cpp
@@ -9,7 +9,19 @@
 using namespace std;
 
 int main() {
-	
+	char input[2];
+	bool play = true;
+	while (play == true) {
+		cout << "Add Vertex(AV), Add Edge(AE), Remove Vertex(RV), Remove Edge(RE), Find Path(FP), Quit: ";
+		cin >> input;
+		cin.get();
+		if (strcmp(input, "AV") == 0 ) {
+			cout << "av" << endl;
+		}
+		if (strcmp(input, "Quit") == 0 ) {
+			play = false;
+		}
+	}
 }
 
 
